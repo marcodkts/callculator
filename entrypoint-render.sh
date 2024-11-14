e63e496894c2739398ec211bf05501a5cfb62817
Marco Aurélio <marco.aurelio_1997@hotmail.com>
2024-11-14 08:46:34 -0300

    fix(entrypoint): fix arguments and imports

--- entrypoint-render.sh
+++ entrypoint-render.sh
@@ -5,7 +5,7 @@ echo ">>> Run Migration"
 python manage.py migrate -v 0
 
 # Create superuser
-echo "from django.conf import settings; from registration.models import User; User.objects.create_superuser(email=settings.DJANGO_SU_EMAIL, password=settings.DJANGO_SU_PASSWORD)" | python manage.py shell
+echo "from django.conf import settings; from django.contrib.auth.models import User; User.objects.create_superuser(username=settings.DJANGO_SU_USERNAME, password=settings.DJANGO_SU_PASSWORD)" | python manage.py shell
 
 # Start Django server
 uvicorn core.asgi:application --host 0.0.0.0 --port 10000

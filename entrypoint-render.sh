1fa7a9823c588c833aa63f3f43afaf70c6d12a47
Marco Aurélio <marco.aurelio_1997@hotmail.com>
2024-11-14 09:07:38 -0300

    fix(entrypoint): fix missing collect static

--- entrypoint-render.sh
+++ entrypoint-render.sh
@@ -4,6 +4,10 @@
 echo ">>> Run Migration"
 python manage.py migrate -v 0
 
+# Run collectstatic
+echo ">>> Collect Static Files"
+python manage.py collectstatic --noinput -v 0
+
 # Create superuser
 echo "from django.conf import settings; from django.contrib.auth.models import User; User.objects.create_superuser(username=settings.DJANGO_SU_USERNAME, password=settings.DJANGO_SU_PASSWORD)" | python manage.py shell
 
